4c210c24601f4a1a54bc2f2da6e05181e88c09d9
baran topal <barantopal@barantopal.com>
2017-07-13 17:13:30 +0200

    initial commit

---
+++ MSMQReceiver/Program.cs
@@ -0,0 +1,58 @@
+﻿/* 
+ * Programmer: Baran Topal                       *
+ * Solution name: MSMQReceiverSender             * 
+ * Project name: MSMQReceiver                    *
+ * File name: Program.cs                         *
+ *                                               *      
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *	                                                                                         *
+ *  LICENSE: This source file is subject to have the protection of GNU General               *
+ *	Public License. You can distribute the code freely but storing this license information. *
+ *	Contact Baran Topal if you have any questions. barantopal@barantopal.com                 *
+ *	                                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+using System;
+using System.Messaging;
+using System.Xml;
+
+namespace MSMQReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (MessageQueue.Exists(@"pc900\private$\queue"))
+            {
+                var msgQ = new MessageQueue(@"pc900\private$\queue");
+                var m = new Message();
+
+                //Use receive() function to receive the message: 
+                m = msgQ.Receive();
+
+                var text = string.Empty;
+                if (m != null)
+                {
+                    //Use XMLMessageFormatter to receive the message without the string wrapper.
+                    m.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+                    text = m.Body.ToString();
+                }
+
+                var xml = new XmlDocument();
+                xml.LoadXml(text);
+                var selectSingleNode = xml.SelectSingleNode("/root/someNode");
+                if (selectSingleNode != null)
+                {
+                    selectSingleNode.InnerText = "baran";
+                }
+                xml.Save(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\received.xml");
+            }
+            else
+            {
+                Console.WriteLine("fail");
+            }
+        }
+    }
+}
---
+++ MSMQSender/Program.cs
@@ -0,0 +1,50 @@
+﻿/* 
+ * Programmer: Baran Topal                       *
+ * Solution name: MSMQReceiverSender             * 
+ * Project name: MSMQSender                      *
+ * File name: Program.cs                         *
+ *                                               *      
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *	                                                                                         *
+ *  LICENSE: This source file is subject to have the protection of GNU General               *
+ *	Public License. You can distribute the code freely but storing this license information. *
+ *	Contact Baran Topal if you have any questions. barantopal@barantopal.com                 *
+ *	                                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+
+using System;
+using System.Messaging;
+using System.Xml;
+
+namespace MSMQSender
+{
+    class MyMessageQueue
+    {
+        private MessageQueue _myNewQueue;
+
+        public void Send()
+        {
+
+            _myNewQueue = new MessageQueue(@"pc900\private$\queue");
+            var mm = new Message();
+
+            var xmlDoc = new XmlDocument { XmlResolver = null };
+            xmlDoc.Load(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\foo.xml");
+            var sXml = xmlDoc.InnerXml;
+
+            mm.Body = sXml;
+
+            _myNewQueue.Send(mm);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myMessageQueue = new MyMessageQueue();
+            myMessageQueue.Send();
+        }
+    }
+}
